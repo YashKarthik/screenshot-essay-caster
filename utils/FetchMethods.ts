d155865dce1693e6a5f578a866220f44e1c6e37e
Yash Karthik <52369876+YashKarthik@users.noreply.github.com>
2022-11-08 10:38:10 +0530

    Forgot to revalidate.

--- utils/FetchMethods.ts
+++ utils/FetchMethods.ts
@@ -3,14 +3,14 @@ import { CastsResponse } from './interfaces';
 export async function getAllEssays(username?: string): Promise<CastsResponse> {
   const query = 'https://searchcaster.xyz/api/search?text=screenshotessay&'
   if (username) {
-    const res = await fetch(query + `username=${username}`);
+    const res = await fetch(query + `username=${username}`, { next: { revalidate: 1800 }});
     return res.json();
   }
-  const res = await fetch(query);
+  const res = await fetch(query, { next: { revalidate: 1800 }});
   return res.json();
 }
 
 export async function getEssayThread(merkleRoot: string): Promise<CastsResponse> {
-  const res = await fetch(`https://searchcaster.xyz/api/search?merkleRoot=${merkleRoot}`);
+  const res = await fetch(`https://searchcaster.xyz/api/search?merkleRoot=${merkleRoot}`, { next: { revalidate: 1800 }});
   return res.json();
 }
