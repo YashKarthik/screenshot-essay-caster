db7cb60331b2c5b6f2f92a0e69051760abfd259d
Yash Karthik <52369876+YashKarthik@users.noreply.github.com>
2022-11-19 06:44:59 +0530

    Responsive.

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default async function Home() {
 
   return (
     <div>
-      <section className="grid grid-cols-4 m-2">
+      <section className="grid gap-4 mx-4 gap-x-11 justify-items-center grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
         {data.casts.map((cast) => {
           if (cast.body.data.image) return <CastCard key='key' cast={cast} />
         })}
