64453d34eb9dc35f070f4a984e30c768ac4a0e93
Yash Karthik <52369876+YashKarthik@users.noreply.github.com>
2022-12-03 19:13:31 +0530

    Update search regex.

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,9 @@ export default async function Home() {
     <div>
       <section className="grid gap-4 mx-4 gap-x-11 justify-items-center grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
         {data.casts.map((cast) => {
-          if (cast.body.data.image) return <CastCard key='key' cast={cast} />
+          if (cast.body.data.image) {
+            return <CastCard key='key' cast={cast} />
+          }
         })}
       </section>
     </div>
--- utils/FetchMethods.ts
+++ utils/FetchMethods.ts
@@ -1,11 +1,7 @@
 import { CastsResponse } from './interfaces';
 
-export async function getAllEssays(username?: string): Promise<CastsResponse> {
-  const query = 'https://searchcaster.xyz/api/search?text=screenshotessay&'
-  if (username) {
-    const res = await fetch(query + `username=${username}`, { next: { revalidate: 1800 }});
-    return res.json();
-  }
+export async function getAllEssays(): Promise<CastsResponse> {
+  const query = 'https://searchcaster.xyz/api/search?regex=screenshotessay|screenshot essay'
   const res = await fetch(query, { next: { revalidate: 1800 }});
   return res.json();
 }
